3d9e9b767f9efd2f84065c20f4c7ec99fb08c1d5
vagrant <vagrant@homestead>
2019-04-22 05:35:34 +0000

    admin

---
+++ app/Admin/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+Admin::registerAuthRoutes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+], function (Router $router) {
+
+    $router->get('/', 'HomeController@index');
+    $router->resource('blog/article', ArticleController::class);
+    $router->resource('blog/category', CategoryController::class);
+
+});
